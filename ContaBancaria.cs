43227c34cb596c81eff51958ca0c431df6c43bd4
lucas apalosqui <lucas.apalosqui@gmail.com>
2022-07-23 13:33:48 -0300

    @lucasApalosqui [TASK: finalização do exercicio de encapsulamento]

---
+++ ContaBancaria.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ConsoleEstudos
+{
+    class ContaBancaria
+    {
+        /*Declaração das variáveis*/
+        private int NumeroConta { get; }
+        public string _titular;
+        private double _saldo;
+        private string _depositoInicial;
+
+
+        /*Construtor que pede 2 parametros para funcionar*/
+        public ContaBancaria(int numeroConta, string titular)
+        {
+            NumeroConta = numeroConta;
+            _titular = titular;
+            _depositoInicial = "n";
+        }
+
+        /*Construtor que pede 3 parametros para funcionar*/
+        public ContaBancaria(int numeroConta, string titular, string depositoInicial)
+        {
+            NumeroConta = numeroConta;
+            _titular = titular;
+            _depositoInicial = depositoInicial;
+
+            if(_depositoInicial == "s")
+            {
+                Console.WriteLine("Informe o valor que deseja depositar: ");
+                DepositaValor(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            }
+        }
+
+
+        /*GET e SET do nome do titular, não deixando ser alterado para valor nulo ou com poucos caracteres*/
+        public string Titular
+        {
+            get { return _titular; }
+            set
+            {
+                if (value != null && value.Length > 2)
+                {
+                    _titular = value;
+                }
+            }
+
+
+        }
+
+        private void MostraSaldo(double saldo)
+        {
+            Console.WriteLine("Olá " + _titular + " portador da conta de numero ( " + NumeroConta + " )\n" + " seu saldo atual é de: R$" + _saldo.ToString("F2", CultureInfo.InvariantCulture));
+        }
+
+        public void DepositaValor(double valor)
+        {
+           
+            _saldo += valor;
+            MostraSaldo(_saldo);
+        }
+
+        public void SacaValor(double valor)
+        {
+            _saldo = (_saldo - 5.0) - valor;
+            MostraSaldo(_saldo);
+        }
+
+
+
+        
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleEstudos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string titular, inicial;
+            double valor;
+            int conta;
+
+            Console.WriteLine("Informe o numero da conta: ");
+            conta = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o nome do titular: ");
+            titular = Console.ReadLine();
+
+            Console.WriteLine("Deseja fazer um depósito inicial? (s/n): ");
+            inicial = Console.ReadLine();
+
+            ContaBancaria contabancaria = new ContaBancaria(conta, titular, inicial);
+
+
+            Console.WriteLine("Qual valor deseja depositar: ");
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            contabancaria.DepositaValor(valor);
+
+            Console.WriteLine("Qual valor deseja sacar: ");
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            contabancaria.SacaValor(valor);
+
+
+
+        }
+    }
+}
