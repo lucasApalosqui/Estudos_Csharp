80f7d9826fd2e8931c8ae691c21e5a1eb08314f0
LucasApalosqui <lucas.apalosqui@gmail.com>
2022-07-26 16:31:09 -0300

    @lucasApalosqui [TASK: ADD Examples for Enums]

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using ProjetoIntermediario.Entities;
+using ProjetoIntermediario.Entities.Enums;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,20 @@ namespace ProjetoIntermediario
     {
         static void Main(string[] args)
         {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.Shipped
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.Delivered.ToString();
+            Console.WriteLine(txt);
+
+            OrderStatus or = (OrderStatus)Enum.Parse(typeof(OrderStatus), "PendingPayment");
+            Console.WriteLine(or);
         }
     }
 }
