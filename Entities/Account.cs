5a52f3b24bb165df5b235faa8ee836e9dfaadf45
LucasApalosqui <lucas.apalosqui@gmail.com>
2022-08-02 17:05:40 -0300

    @lucasApalosqui[TASK: Exception / Exception Personalizado]

---
+++ Entities/Account.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+using ExceptionPersonalizadaAccount.Entities.Exceptions;
+
+namespace ExceptionPersonalizadaAccount.Entities
+{
+    internal class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account()
+        {
+        }
+
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public override string ToString()
+        {
+            return "New balance: "
+                + Balance.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public void Deposit(double amont)
+        {
+
+            Balance += amont;
+        }
+
+        public void Withdraw(double amont)
+        {
+            if(amont > Balance)
+            {
+                throw new DomainException("Withdraw error: Not enough balance");
+            }
+            if(amont > WithdrawLimit)
+            {
+                throw new DomainException("Withdraw error: The amount exceeds withdraw limit");
+            }
+            Balance -= amont;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using ExceptionPersonalizadaAccount.Entities;
+using ExceptionPersonalizadaAccount.Entities.Exceptions;
+
+namespace ExceptionPersonalizadaAccount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string name = Console.ReadLine();
+                Console.Write("Initial Balance: ");
+                double initialBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit: ");
+                double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account account = new Account(number, name, initialBalance, withdrawLimit);
+                Console.WriteLine();
+                Console.Write("Enter amount to withdraw: ");
+                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                account.Withdraw(amount);
+                Console.WriteLine(account);
+
+
+            }
+            catch(DomainException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}
