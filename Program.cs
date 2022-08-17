7656883e95c55da3c84ee870cdd62508583fd10f
LucasApalosqui <lucas.apalosqui@gmail.com>
2022-08-17 12:42:48 -0300

    @lucasApalosqui [TASK: exemplo de delegates e multi delegates]

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace DelegatesAndMulticastDelegates;
+using DelegatesAndMulticastDelegates.Services;
+
+internal class Program
+{
+    delegate void PrintContent(string a, string b);
+    delegate string JuntaNomes(string a, string b);
+    static void Main(string[] args)
+    {
+        PrintContent print = StringAlterService.trocaPalavra;
+        JuntaNomes junta = StringAlterService.juntaNomes;
+
+        print += StringAlterService.juntaPalavrinhas;
+        string palavraUnida = junta.Invoke("barraquinha", "de milhões");
+
+        print.Invoke("pao", "saladinha");
+        Console.WriteLine(palavraUnida);
+
+        
+    }
+}
---
+++ Services/StringAlterService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndMulticastDelegates.Services
+{
+    internal class StringAlterService
+    {
+        public static void trocaPalavra(string palavra, string novaPalavra)
+        {
+            Console.WriteLine(palavra.Replace(palavra, novaPalavra));
+        }
+
+        public static void juntaPalavrinhas(string palavra, string letra)
+        {
+            Console.WriteLine(palavra + letra);
+        }
+
+        public static string juntaNomes(string nome1, string nome2)
+        {
+            return nome1 + " " + nome2;
+        }
+    }
+}
