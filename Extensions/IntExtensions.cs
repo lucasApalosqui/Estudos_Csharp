7f1d211d59a5697f0ac1a9ceb96dc6144c450b14
LucasApalosqui <lucas.apalosqui@gmail.com>
2022-08-17 10:57:42 -0300

    @lucasApalosqui [TASK: exemplo prático de metodos de extensão]

---
+++ Extensions/IntExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionsMethods.Extensions
+{
+    static class IntExtensions
+    {
+        public static string countNumbers(this int thisObj, int number)
+        {
+            string result = "";
+            int teste = number;
+            int totalNum = 0;
+            if(number == 0 || number > thisObj || thisObj == 0)
+            {
+                return "it´s not possible!";
+            }
+            for(int i = number; i <= thisObj; i+= number)
+            {
+                if(teste == i)
+                {
+                    result = "/";
+                }
+                result = result + number + "/";
+                totalNum++;
+            }
+            return result + "\n total occurrences: ( " + totalNum + " )";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ExtensionsMethods;
+using ExtensionsMethods.Extensions;
+internal class Program
+{
+    static void Main(string [] args)
+    {
+        Console.Write("Qual numero deseja analisar: ");
+        int num = int.Parse(Console.ReadLine());
+
+        Console.Write("Qual numero de vezes: ");
+        int numV = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(num.countNumbers(numV));
+    }
+}
